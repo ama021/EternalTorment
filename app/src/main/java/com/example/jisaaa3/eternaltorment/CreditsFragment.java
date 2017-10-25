8a19dabad52dc0d2f9a6a4385c78560b903f8c48
Adrian <ama021@uark.edu>
2017-10-25 17:44:30 -0500

    Credits Fragment creation

---
+++ app/src/main/java/com/example/jisaaa3/eternaltorment/CreditsFragment.java
@@ -0,0 +1,29 @@
+package com.example.jisaaa3.eternaltorment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Adrian on 10/25/2017.
+ */
+
+public class CreditsFragment extends Fragment{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.credits_fragment, container, false);
+
+
+
+        return v;
+    }
+}
