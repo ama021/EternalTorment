0721ff8c846cc0383550fc154adc7f8a18f7eb19
ama021 <ama021@uark.edu>
2017-10-26 11:33:34 -0500

    Adding Recycler View functionality to the Armory

--- app/src/main/java/com/example/jisaaa3/eternaltorment/ArmoryActivity.java
+++ app/src/main/java/com/example/jisaaa3/eternaltorment/ArmoryActivity.java
@@ -2,12 +2,72 @@ package com.example.jisaaa3.eternaltorment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class ArmoryActivity extends AppCompatActivity {
+    private RecyclerView mArmoryRecyclerView;
+    private ArmoryItemAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_armory);
+
+        mArmoryRecyclerView = (RecyclerView) findViewById(R.id.armory_recycler_view);
+        mArmoryRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        updateUI();
+    }
+
+    private void updateUI() {
+
+
+    }
+
+    private class ArmoryItemHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public ArmoryItemHolder(View view) {
+            super(view);
+            itemView.setOnClickListener(this);
+        }
+
+        public void bind(ArmoryItem item) {
+
+        }
+
+        @Override
+        public void onClick(View view) {
+            //Show clicked on results in other frame layouts
+            //Broadcast receiver? to let other frame layouts know?
+        }
+    }
+
+    private class ArmoryItemAdapter extends RecyclerView.Adapter<ArmoryItemHolder> {
+
+        public ArmoryItemAdapter() {
+
+        }
+
+        @Override
+        public ArmoryItemHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.armorylist_item, parent, false);
+            return new ArmoryItemHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(ArmoryItemHolder holder, int poistion) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 5;
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/jisaaa3/eternaltorment/ArmoryItem.java
@@ -0,0 +1,8 @@
+package com.example.jisaaa3.eternaltorment;
+
+/**
+ * Created by jisaaa3 on 10/26/2017.
+ */
+
+public class ArmoryItem {
+}
