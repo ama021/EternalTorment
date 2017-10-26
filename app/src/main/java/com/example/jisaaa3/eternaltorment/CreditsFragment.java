768fabe650d79520b6e8f86e06111e4b139567d5
Adrian <ama021@uark.edu>
2017-10-25 22:50:09 -0500

    Support to switch back to main menu fragment

--- app/src/main/java/com/example/jisaaa3/eternaltorment/CreditsFragment.java
+++ app/src/main/java/com/example/jisaaa3/eternaltorment/CreditsFragment.java
@@ -6,12 +6,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 /**
  * Created by Adrian on 10/24/2017.
  */
 
 public class CreditsFragment extends Fragment{
+    private LinearLayout mCreditsContainer;
+
+    private int mFragmentContainer;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +28,20 @@ public class CreditsFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.credits_fragment, container, false);
 
+        mFragmentContainer = getArguments().getInt(getString(R.string.fragmentContainer));
 
+        mCreditsContainer = (LinearLayout) v.findViewById(R.id.credits_container);
+        mCreditsContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().onBackPressed();
+
+                /*
+                    Ask about increasing the code cache capacity each time fragments
+                    switch
+                 */
+            }
+        });
 
         return v;
     }
