1292806150706006de359161ad1d4805ec98cd2b
Jtharden <jtharden@uark.edu>
2017-12-04 02:11:57 -0600

    I commited even more shit!

--- app/src/main/java/com/example/jisaaa3/eternaltorment/Skeleton.java
+++ app/src/main/java/com/example/jisaaa3/eternaltorment/Skeleton.java
@@ -9,7 +9,7 @@ import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Random;
 
 
 /**
@@ -20,9 +20,7 @@ public class Skeleton extends Sprite {
 
     private static final String TAG = "Skeleton";
 
-
-    private int shield_side;
-    private int skeleton_corridor;
+    private Context mContext;
 
     private Rect skeletonBoundary;
 
@@ -31,8 +29,16 @@ public class Skeleton extends Sprite {
     private int screensize_x;
     private int screensize_y;
 
+    // Randomized variables
+    Random random;
+    private boolean shieldRight = true;
+    private int skeletonCoridore = 0;
+
     public Skeleton(Context context, int screensize_x, int scrrensize_y) {
         setType("Skeleton");
+        this.mContext = context;
+        random = new Random();
+        random_direction();
 
         this.setAlive(true);
 
@@ -46,6 +52,8 @@ public class Skeleton extends Sprite {
 
         this.skeletonBoundary = new Rect();
         setSkeletonBoundary();
+
+
     }
 
     public boolean update(long fps, List<Sprite> spriteList) {
@@ -80,5 +88,65 @@ public class Skeleton extends Sprite {
 
         this.setSpriteHitBox(this.skeletonBoundary);
         Log.d(TAG, "Skeleton Boundary x.left " + skeletonBoundary.left + " top: " + skeletonBoundary.top + " bottom: " + skeletonBoundary.bottom + " right: " + skeletonBoundary.right);
+    }
+
+    public void random_direction(){
+        int n = random.nextInt(4);
+        int m = random.nextInt(2);
+
+        Resources r = mContext.getResources();
+        // Skeleton Top
+        if(n == 0)
+        {
+            // Shield side
+            if(m == 0){
+                shieldRight = true;
+                //setCurrentBitmap(BitmapFactory.decodeResource(r, ));
+            }else{
+                shieldRight = false;
+            }
+
+            this.getLocation().x = screensize_x;
+            this.getLocation().y = 0 - currentBitmap.getHeight();
+        }
+
+        // Skeleton Bottom
+        else if(n == 1){
+            // Shield side
+            if(m == 0){
+                shieldRight = true;
+            }else{
+                shieldRight = false;
+            }
+
+            this.getLocation().x = screensize_x;
+            this.getLocation().y = screensize_y * 2;
+        }
+
+        // Skeleton Left
+        else if(n == 2){
+            // Shield side
+            if(m == 0){
+                shieldRight = true;
+            }else{
+                shieldRight = false;
+            }
+
+            this.getLocation().x = 0 - currentBitmap.getHeight();
+            this.getLocation().y = screensize_y;
+        }
+
+        // Skeleton Right
+        else{
+            // Shield side
+            if(m == 0){
+                shieldRight = true;
+            }else{
+                shieldRight = false;
+            }
+
+            this.getLocation().x = screensize_x * 2;
+            this.getLocation().y = screensize_y;
+        }
     }
 }
